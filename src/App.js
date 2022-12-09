15b7ca19cd211e5e5a1a3f61a91fb49950e5260b
scokljat <selma.cokljat@hotmail.com>
2022-12-09 21:31:01 +0100

    Changed default route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Movies from "./pages/movies/Movies";
 import TvShows from "./pages/tv-shows/TvShows";
 import Detail from "./pages/movies/detail/Detail";
@@ -10,6 +10,7 @@ function App() {
     <div className="wrapper">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/movies" />} />
         <Route path="/movies" element={<Movies />} />;
         <Route path="/movies/:id" element={<Detail />} />
         <Route path="/tv-shows" element={<TvShows />} />;
