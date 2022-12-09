004d2fbc7dbdcad589c698adeba64911a96bea27
scokljat <selma.cokljat@hotmail.com>
2022-12-09 11:01:46 +0100

    Added  card

---
+++ src/components/card/Card.js
@@ -0,0 +1,17 @@
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="cardWrapper">
+      <img
+        src="https://imgv3.fotor.com/images/homepage-feature-card/enhance-photo-of-three-girls-playing-in-the-sunflower-garden.png"
+        alt="movie"
+        width="100%"
+        height="250px"
+      />
+      <h1> Movie title</h1>
+    </div>
+  );
+}
+
+export default Card;
